0a197a20e8f28f85d58c205360192dc5ea4a5630
hritik saini <sainihritik033@gmail.com>
2025-12-05 17:51:32 +0530

    fix(types): fix the slug and fix the pageId

---
+++ src/app/[[...slug]]/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useQuery } from "@apollo/client/react";
+import { GET_PAGE } from "@/lib/graphql/queries";
+import { useParams } from "next/navigation";
+
+export default function DynamicPage() {
+  const params = useParams();
+
+  let pageId = "home";
+
+  if (params?.slug) {
+    pageId = Array.isArray(params.slug)
+      ? params.slug.join("/")
+      : params.slug;
+  }
+
+  const { data, loading, error } = useQuery(GET_PAGE, {
+    variables: { pageId }
+  });
+
+
+  if (loading) return <p>Loading…</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <>
+      <h1>TDACorp - Studio</h1>
+      <div>
+        <h1>Products from API</h1>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    </>
+  );
+}
